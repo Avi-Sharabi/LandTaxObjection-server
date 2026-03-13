2eb05fd21002cd33c64e6a30e55abfe2228e31c8
Pol Imbing <pol.imbing@ymlgroup.com.au>
2026-03-13 13:06:59 +0800

    increase limit  for url encoded

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { json } from 'express';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
 
@@ -22,9 +22,10 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
-
-  await app.listen(process.env.PORT ?? 3000);
   app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+  await app.listen(process.env.PORT ?? 3000);
+
   console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
   console.log(`Swagger docs: http://localhost:${process.env.PORT ?? 3000}/api/docs`);
 }
