bacee5d35de4eae5394dbf779494964c6dfd04eb
Pol Imbing <pol.imbing@ymlgroup.com.au>
2026-04-30 12:25:21 +0800

    feat(queue): update Redis configuration to include password and TLS support

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -7,15 +7,23 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     BullModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        connection: {
-          host: config.get<string>('REDIS_HOST', 'localhost'),
-          port: config.get<number>('REDIS_PORT', 6379),
-        },
-      }),
+      useFactory: (config: ConfigService) => {
+        const isProd = config.get<string>('NODE_ENV') === 'production';
+
+        return {
+          connection: {
+            host: config.get<string>('REDIS_HOST'),
+            port: config.get<number>('REDIS_PORT'),
+            ...(isProd && {
+              password: config.get<string>('REDIS_PASSWORD'),
+              tls: {},
+            }),
+          },
+        };
+      },
       inject: [ConfigService],
     }),
   ],
   exports: [BullModule],
 })
-export class QueueModule {}
+export class QueueModule { }
